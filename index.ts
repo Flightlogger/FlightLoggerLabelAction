5a259457f0ab9cdca0c82bcafeb1cb2ae596371f
Kåre Jensen <jensenks@gmail.com>
2020-06-10 11:34:14 +0200

    Add review labels when converting from draft to regular PR

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const LINKED_ISSUES_REGEX = /(close|closes|closed|fix|fixes|fixed|resolve|resolv
 const REGEX_MATCH_ID_INDEX = 2;
 const PULL_REQUEST_EVENT = "pull_request";
 const PULL_REQUEST_REVIEW_EVENT = "pull_request_review";
+const PULL_REQUEST_READY_FOR_REVIEW = "ready_for_review";
 const REVIEW_LABEL_ACTIONS = ["opened", "edited"];
 const MERGE_LABEL_ACTIONS = ["submitted"];
 const APPROVED_STATE = "approved";
@@ -40,8 +41,13 @@ async function applyReviewLabels(client: github.GitHub, payload: WebhookPayload)
   const reviewLabel = core.getInput('review-label', {required: true});
   const reviewTrigger = core.getInput('review-trigger', {required: true});
   const pullRequest = payload.pull_request;
+  if (payload.action == PULL_REQUEST_READY_FOR_REVIEW) {
+    console.log(`Draft PR marked as ready for review`);
+    await labelPRAndLinkedIssues(client, payload, reviewLabel);
+    return;
+  }
 
-  if(pullRequest.body.toLowerCase().includes(reviewTrigger.toLowerCase())) {
+  if (pullRequest.body.toLowerCase().includes(reviewTrigger.toLowerCase())) {
     console.log(`Found review trigger in PR body: ${reviewTrigger}`);
     await labelPRAndLinkedIssues(client, payload, reviewLabel);
   }
